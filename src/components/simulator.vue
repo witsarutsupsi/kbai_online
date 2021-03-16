d031da2fed660ca30d8692d45cb98f35d69dd72f
admin <admin@gmail.com>
2021-03-16 12:30:18 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="sim-panel d-flex justify-content-center">Virtual Kanomchan</div>
+  <div class="sim-panel d-flex justify-content-center">
+    <ul>
+
+       <li class="project">   
+         <a :href="publicPath + 'src/assets/UnityBuild/index.html'">Virtual Kanomchan</a> 
+       </li>
+
+    </ul>
+  </div>
 </template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      publicPath: process.env.BASE_URL
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .sim-panel {
   background-color: #fff2d6;
   padding: 20px;
-  margin: 35px;
+  margin-top: 35px;
+  margin-left: auto;
   width: 1280px;
   height: 720px;
 }
