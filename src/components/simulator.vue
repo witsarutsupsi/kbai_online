8992f8d1e9c23c873482df7ee5a04138b511ceaa
admin <admin@gmail.com>
2021-03-17 13:28:39 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sim-panel d-flex justify-content-center">Virtual Kanomchan
-  <object data="../assets/UI/png/Group 13.png" height="300" width="500"></object>
+  <object data="./components/UnityBuild/index.html" height="300" width="500"></object>
   </div>
 </template>
 
