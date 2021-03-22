29fc2de42cf4a8ee677011500140ac5c28cdb602
admin <admin@gmail.com>
2021-03-22 12:26:46 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sim-panel d-flex justify-content-center">VK
-  <object data="https://www.kid-bright.org/" height="300" width="500"></object>
+  <object data="UnityBuild/index.html" height="300" width="500"></object>
   </div>
 </template>
 
