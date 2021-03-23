ec554dc75576ece6d0d6676de8494398f25aaf7b
admin <admin@gmail.com>
2021-03-23 09:07:34 +0700

    add

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,30 @@
 module.exports = {
-    devServer: {
-	hot: true,
+  devServer: {
+	  hot: true,
     liveReload: false,
-        proxy: 'http://172.20.10.2:3000'
-    },
-    css: { extract: false },
-    assetsDir: 'static',
-    configureWebpack:{
-        devtool: 'source-map',
-        optimization: {
-          splitChunks: {
-            minSize: 10000,
-            maxSize: 250000,
-          }
-        }
-      },
-      productionSourceMap: true, // NOTE: this is default
+    proxy: 'http://172.20.10.2:3000'
+  },
+  css: { extract: false },
+  assetsDir: 'static',
+  configureWebpack:{
+    devtool: 'source-map',
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
+    }
+  },
+
+
+  chainWebpack: config => {
+    config.module
+    .rule('html')
+    .test(/\.html$/)
+    .use('html-loader')
+    .loader('html-loader')
+  }
+
+  productionSourceMap: true, // NOTE: this is default
 
 }
