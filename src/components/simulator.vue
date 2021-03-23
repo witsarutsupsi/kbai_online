ba71a6870ec895f8336e8bf4548cfe1844828ec8
admin <admin@gmail.com>
2021-03-23 09:51:02 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="sim-panel d-flex justify-content-center">
+  <div id="webgl">VK</div>
   </div>
 </template>
 
 <script>
+import html from '@/components/UnityBuild/index.html'
 
+export default {
+  data() {
+    return {
+      html,   // es6 property shorthand syntax
+      foo: null
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
