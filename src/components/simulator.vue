52e5c650b2359a863c1e6f4302d3a0a974fc353a
admin <admin@gmail.com>
2021-03-24 12:41:34 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,19 +1,27 @@
 <template>
-  <div id="sim-panel">
-  <VK msg="Welcome to Your Vue.js App" />
+  <div class="sim-panel">
+    <p id="data"></p>
   </div>
 </template>
 
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
-import VK from "./UnityBuild/vk.vue";
 
-export default {
-  name: "sim-panel",
-  UnityBuild: {
-    VK,
-  },
-};
+function callback(res){
+            document.getElementById("data").value = res.IPv4;
+        }
+        $.ajax({
+            url:"https://geoip-db.com/jsonp/",
+            dataType:"jsonp"
+        })
 </script>
 
-
+<style>
+.vk-panel {
+  background-color: #fff2d6;
+  padding: 20px;
+  margin: auto;
+  width: 1280px;
+  height: 720px;
+}
+</style>
