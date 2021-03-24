505532d3ceec3cb07df44320d8bef4a2571d0c31
admin <admin@gmail.com>
2021-03-24 14:47:20 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,24 +1,12 @@
 <template>
   <div class="sim-panel">
-    <link rel="shortcut icon" href="TemplateData/favicon.ico">
-    <link rel="stylesheet" href="TemplateData/style.css">
-    <script src="TemplateData/UnityProgress.js"></script>
-    <script src="src/FileSaver.js"></script>
-    <script src="Build/UnityLoader.js"></script>
-    <div id="gameContainer" style="width: 990px; height: 557px"></div>
+  
   </div>
 </template>
 
 
 <script>
-export default {
-      data () {
-        return {
-            var gameInstance = UnityLoader.instantiate("gameContainer", "Build/VKBuild.json", {onProgress: UnityProgress});
-          }
-        }
-      }
-    } 
+
 </script>
 
 <style>
