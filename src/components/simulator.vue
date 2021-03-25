91eac3f78da22d864aea912a14d7bc683e9e9d2c
admin <admin@gmail.com>
2021-03-25 09:17:20 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -24,7 +24,7 @@ export default {
 
 <style>
 .sim-panel {
-  background-color: #111111;
+  background-color: #fff7d6;
   margin: auto;
   width: 1280px;
   height: 720px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,17 @@
   <div id="app">
     <Main msg="Welcome to Your Vue.js App" />
     <Simulator msg="Welcome to Your Vue.js App" />
-    <unity
-        src="/Build/VKBuild.json"
-        width="1000"
-        height="600"
-        unityLoader="/Build/UnityLoader.js" ref="myInstance"
-      >
-     </unity>
   </div>
 </template>
 
 <script>
-import Unity from 'vue-unity-webgl'
 import Main from "../components/main.vue";
 import Simulator from "../components/simulator.vue";
 
 export default {
   name: "app",
   components: {
-    Main, Simulator, Unity
+    Main, Simulator
   },
 };
 </script>
