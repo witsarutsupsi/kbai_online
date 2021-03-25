3a3d0ffab8bdd39820dc6b35e7c9616c12e305eb
admin <admin@gmail.com>
2021-03-25 08:59:17 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,16 +1,30 @@
 <template>
     <div id="sim-panel">
+    <unity
+        src="/Build/VKBuild.json"
+        width="1000"
+        height="600"
+        unityLoader="/Build/UnityLoader.js" ref="myInstance"
+      >
+     </unity>
     </div>
 </template>
 
 
 <script>
+import Unity from 'vue-unity-webgl'
 
+export default {
+  name: "app",
+  components: {
+    Unity
+  },
+};
 </script>
 
 <style>
 .sim-panel {
-  background-color: #735336;
+  background-color: #111111;
   margin: auto;
   width: 1280px;
   height: 720px;
