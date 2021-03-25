6151b035ea2c3a366760ac3c64a409dc9e467106
admin <admin@gmail.com>
2021-03-25 14:56:18 +0700

    add

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <main v-if="!isLoading">
         <Main msg="Welcome to Your Vue.js App" />
         <Simulator msg="Welcome to Your Vue.js App" />
-        <unity src="/Build/VKBuild.json" width="1000" height="600" unityLoader="/Build/UnityLoader.js" ref="myInstance"></unity>
+        <unity src="/assets/Build/VKBuild.json" width="1000" height="600" unityLoader="/assets/Build/UnityLoader.js" ref="myInstance"></unity>
     </main> -->
 
     <router-view />
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import Unity from 'vue-unity-webgl'
+import unity from 'vue-unity-webgl'
 import Main from './components/main.vue'
 import Simulator from './components/simulator.vue'
 import LoadingScreen from "./components/LoadingScreen";
@@ -27,7 +27,7 @@ export default {
     name: 'app',
     components: {
         Main, Simulator,
-        LoadingScreen, Unity
+        LoadingScreen, unity
     },
     data() {
         return {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,19 @@
   <div id="app">
     <Main msg="Welcome to Your Vue.js App" />
     <Simulator msg="Welcome to Your Vue.js App" />
-    <unity src="/Build/VKBuild.json" width="1000" height="600" unityLoader="/Build/UnityLoader.js" ref="myInstance"></unity>
+    <unity src="/assets/Build/VKBuild.json" width="1000" height="600" unityLoader="/assets/Build/UnityLoader.js" ref="myInstance"></unity>
   </div>
 </template>
 
 <script>
 import Main from "../components/main.vue";
 import Simulator from "../components/simulator.vue";
-import Unity from 'vue-unity-webgl'
+import unity from 'vue-unity-webgl'
 
 export default {
   name: "app",
   components: {
-    Main, Simulator, Unity
+    Main, Simulator, unity
   },
 };
 </script>
