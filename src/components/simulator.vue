2eab0657b0b4d4fe6fe36c6f2842504e3647ff46
admin <admin@gmail.com>
2021-03-26 13:36:01 +0700

    add

--- src/components/simulator.vue
+++ src/components/simulator.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="sim-panel font-weight-bold">Virtual Kanomchan
-		<unity src="static/Build/VKBuild.json" width="1000" height="600" unityLoader="static/Build/UnityLoader.js" ref="myInstance"></unity>
+		<unity src="assets/Build/VKBuild.json" width="1000" height="600" unityLoader="assets/Build/UnityLoader.js" ref="myInstance"></unity>
     </div>
 </template>
 
@@ -8,16 +8,16 @@
 <script>
 import Unity from 'vue-unity-webgl'
 
-export default {
-  name: "app",
-  components: {
-    Unity
-  },
-};
+new Vue({
+    components: { Unity }
+  })
 </script>
 
 <style>
 .sim-panel {
+  color: #06754b;
+  font-size: 35px;
+  text-align: center;
   background-color: #fff7d6;
   margin: auto;
   width: 1280px;
